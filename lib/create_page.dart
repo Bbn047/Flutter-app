13999b4710f80a3aa32d6bd18dcd315818b9066c
Bibin Thomas <bibinthomas4897@gmail.com>
2025-07-31 13:09:08 +0200

    Update create_page.dart

--- lib/create_page.dart
+++ lib/create_page.dart
@@ -2,4 +2,16 @@ import 'package:flutter/material.dart';
 
 class CreatePage extends StatelessWidget {
   final TextEditingController nameController = TextEditingController();
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Center(
+        child: Text('This is your Dashboard', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
 }
